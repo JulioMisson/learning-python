5cdf4bc2a2c03b71567c5e5b864bf5d968a3b233
JulioMisson <159930590+JulioMisson@users.noreply.github.com>
2025-09-10 21:20:02 -0300

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,25 @@
+#entendendo strings
+
+#cocatenaçao
+word_1 = 'SUPER'
+word_2 = 'BOTO'
+print(word_1 + word_2)
+
+#repetiçao
+w= "w"
+rep= w *3
+print(rep)
+
+#comparaçao
+
+a="a"
+b="b"
+if a != b:
+    print("i like you")
+else:
+    print("i dont ike you")
+
+#formataçao
+flag = True
+n= 10
+print(f'numero:{n}, flag: {flag}')
